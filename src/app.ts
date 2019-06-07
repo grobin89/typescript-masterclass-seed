73d054f5233fec18ce34d32f4773bf143ed4e407
grobin89 <grobin89@gmail.com>
2019-06-07 11:34:35 +1000

    Base example

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,9 @@
 interface Person {
     name: string;
-    age?: number;
-}
-
-type MyRequired<T> = {
-    [P in keyof T]-?: T[P]
-};
-
-function printAge(person: Required<Person>) {
-    return `${person.name} is ${person.age}`;
+    age: number;
 }
 
 const person: Required<Person> = {
     name: 'Greg',
     age: 30
 }
-
-console.log(printAge(person));
