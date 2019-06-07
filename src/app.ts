5beb9ea4500c78df7487b27dfe0952bdbfeee5f4
grobin89 <grobin89@gmail.com>
2019-06-07 12:11:36 +1000

    record mapped type

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-interface Person {
-    name: string;
-    age: number;
+let dictionary: Record<string, TrackStates> = {};
+
+interface TrackStates {
+    current: string;
+    next: string;
 }
 
-const person: Required<Person> = {
-    name: 'Greg',
-    age: 30
-}
+const item: Record<keyof TrackStates, string> = {
+    current: 'fr23dsdc',
+    next: 'dffdr34t',
+};
+
+// Numbers are coerced to String
+dictionary[0] = item;
